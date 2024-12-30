d12f6d785ebbb5ee98e9cfa7a83e053ba11e1d2a
Paul Coroneos <5339+coroneos@users.noreply.github.com>
2024-12-29 23:54:24 -0600

    Rate Limit

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -20,6 +21,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
@@ -30,11 +32,13 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-
         if keys[pygame.K_SPACE]:
             self.shoot(dt)
 
     def shoot(self, dt):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         self.shot = Shot(self.position.x, self.position.y)
         self.shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
